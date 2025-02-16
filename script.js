cd392fdac0808c550bf27ef2124de45f65ea6827
Abhishek Kumar Patel <abhishekpatel12131@gmail.com>
2025-02-17 01:13:18 +0530

    loading screen added

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // glow on hover
-
 let cards = document.querySelectorAll(".toolCard");
 
 cards.forEach(card => {
@@ -27,10 +26,8 @@ cards.forEach(card => {
     });
 });
 
-
+// _________________________________________________________________________________________
 //project video
-
-
 const workContainers = document.querySelectorAll('.work1');
 
 workContainers.forEach(container => {
@@ -47,12 +44,13 @@ workContainers.forEach(container => {
   }
 });
 
-
+// _________________________________________________________________________________________
 // background music
 
 const audio = document.getElementById("backAudio");
-let audioContext;
+audio.loop = true; // Enable looping
 
+let audioContext;
 function toggleAudio() {
     if (!audioContext) {
         audioContext = new (window.AudioContext || window.webkitAudioContext)();
@@ -73,8 +71,8 @@ function toggleAudio() {
 
 
 
-// Dropdowns
 
+// Dropdowns
 const accordionButtons = document.querySelectorAll('.accordion-button');
 
 accordionButtons.forEach((button) => {
@@ -96,7 +94,7 @@ accordionButtons.forEach((button) => {
   });
 });
 
-
+// _________________________________________________________________________________________
 // back to top button in smooth scroll
 
 
@@ -120,6 +118,7 @@ accordionButtons.forEach((button) => {
   });
 
 
+// _________________________________________________________________________________________
 // Custom cursor
 
 
@@ -183,4 +182,97 @@ function onMouseHoverOut() {
     $bigBall.classList.remove('behind');
 }
 
+// _________________________________________________________________________________________
+// Loading Animation
 
+
+function Ticker(elem) {
+  elem.lettering();
+  this.done = false;
+  this.cycleCount = 5;
+  this.cycleCurrent = 0;
+  this.chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_=+{}|[]\\;\':"<>?,./`~'.split('');
+  this.charsCount = this.chars.length;
+  this.letters = elem.find('span');
+  this.letterCount = this.letters.length;
+  this.letterCurrent = 0;
+
+  this.letters.each(function() {
+    var $this = $(this);
+    $this.attr('data-orig', $this.text());
+    $this.text('-');
+  });
+}
+
+Ticker.prototype.getChar = function() {
+  return this.chars[Math.floor(Math.random() * this.charsCount)];
+};
+
+Ticker.prototype.reset = function() {
+  this.done = false;
+  this.cycleCurrent = 0;
+  this.letterCurrent = 0;
+  this.letters.each(function() {
+    var $this = $(this);
+    $this.text($this.attr('data-orig'));
+    $this.removeClass('finished');
+  });
+  this.loop();
+};
+
+Ticker.prototype.loop = function() {
+  var self = this;
+
+  this.letters.each(function(index, elem) {
+    var $elem = $(elem);
+    if (index >= self.letterCurrent) {
+      if ($elem.text() !== ' ') {
+        $elem.text(self.getChar());
+        $elem.css('opacity', Math.random());
+      }
+    }
+  });
+
+  if (this.cycleCurrent < this.cycleCount) {
+    this.cycleCurrent++;
+  } else if (this.letterCurrent < this.letterCount) {
+    var currLetter = this.letters.eq(this.letterCurrent);
+    this.cycleCurrent = 0;
+    currLetter.text(currLetter.attr('data-orig')).css('opacity', 1).addClass('finished');
+    this.letterCurrent++;
+  } else {
+    this.done = true;
+  }
+
+  if (!this.done) {
+    requestAnimationFrame(function() {
+      self.loop();
+    });
+  } else {
+    setTimeout(function() {
+      self.reset();
+    }, 750);
+  }
+};
+
+
+$(document).ready(function() {
+  $('html, body').addClass('loading');
+  
+  var $loaders = $('.loader');
+  
+  $loaders.each(function() {
+    var $this = $(this);
+    var ticker = new Ticker($this).reset();
+    $this.data('ticker', ticker);
+  });
+
+  // Hide loader after page is fully loaded
+  $(window).on('load', function() {
+    setTimeout(function() {
+      $('.loader, .backdrop').fadeOut('slow');
+      // Remove loading class from html and body
+      $('html, body').removeClass('loading');
+    }, 2000);
+  });
+});
